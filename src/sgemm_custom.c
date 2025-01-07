2a84f1af6d601bdf7fdee8c915d01646579bbec1
Jetson NXC <nxclab@gmail.com>
2025-01-07 17:47:25 +0900

    prefetched. performance degraded

--- src/sgemm_custom.c
+++ src/sgemm_custom.c
@@ -52,7 +52,7 @@ void pack_B(int incRowB, int incColB, float* B, float* _B){
     }
 }
 
-void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float alpha, float beta){
+void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float alpha, float beta, float *nextA, float *nextB){
     int k = KC/4;
     int kr = KC%4;
 
@@ -61,6 +61,8 @@ void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float 
         "ldr    x1, %[B] \n\t"   // load B into x1
         "mov    x3, %[k]    \n\t" 
         "mov    x4, %[kr]   \n\t"
+        "ldr    x5, %[nextA]    \n\t"
+        "ldr    x6,  %[nextB]    \n\t"
         "ld1    {v0.4s}, [x1] \n\t" // btmp = vld1q_f32(B);
         "ld1    {v1.4s}, [x0] \n\t" // atmp0 = vld1q_f32(A);
         "eor    v10.16b, v10.16b, v10.16b \n\t" // ab_00 = vdupq_n_f32(0.0f);
@@ -69,7 +71,11 @@ void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float 
         "eor    v13.16b, v13.16b, v13.16b \n\t" // ab_10 = vdupq_n_f32(0.0f);
         "                                 \n\t"
             ".DLOOP%=:          \n\t"
-            "                   \n\t"                                                                           // update 1
+            "                   \n\t"    
+            "prfm   pldl1keep,  [x6]            \n\t" // PREFETCH
+            "add    x6, x6, 64                  \n\t"  
+            "prfm   pldl1keep,  [x5]            \n\t" // PREFETCH
+            "add    x5, x5, 64                  \n\t"                                                              // update 1
             "ext    v2.16b, v1.16b, v1.16b, 12  \n\t" // atmp3 = vextq_f32(atmp0, atmp0, 3);
             "ext    v3.16b, v1.16b, v1.16b, 8   \n\t" // atmp2 = vextq_f32(atmp0, atmp0, 2);
             "ext    v4.16b, v1.16b, v1.16b, 4   \n\t" // atmp1 = vextq_f32(atmp0, atmp0, 1);
@@ -127,6 +133,10 @@ void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float 
             "fmla   v13.4s, v4.4s,  v0.4s       \n\t"
             "                                   \n\t"
             "ld1    {v0.4s}, [x1]               \n\t" // btmp = vld1q_f32(B); B += 4;
+            "prfm   pldl1keep,  [x6]            \n\t" // PREFETCH
+            "add    x6, x6, 64                  \n\t" 
+            "prfm   pldl1keep,  [x5]            \n\t" // PREFETCH
+            "add    x5, x5, 64                  \n\t" 
             "                                   \n\t"
             "subs   x3,  x3,    1               \n\t" // k--
             "bne    .DLOOP%=                    \n\t"
@@ -157,7 +167,6 @@ void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float 
         "mov    w3, %w[alpha]    \n\t" 
         "mov    w4, %w[beta]   \n\t"
         "mov    x0, %[incRowC]    \n\t" 
-        // float byte 만큼 곱해줘야함.
         "dup    v0.4s,  w3      \n\t" // duplicated alpha
         "dup    v1.4s,  w4      \n\t" // duplicated beta
 
@@ -170,7 +179,7 @@ void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float 
         "fmla   v2.4s,  v4.4s, v0.4s    \n\t"
         "st1    {v2.4s},    [x2]        \n\t"
 
-        "mov    x0,     x0,     lsl 2       \n\t"
+        "mov    x0,     x0,     lsl 2       \n\t" // incRowC *= sizeof(float)
         "add    x2,     x2,     x0      \n\t"
         
         "ins    v4.s[0],    v13.s[0]    \n\t"
@@ -203,28 +212,40 @@ void micro_kernel(float *A, float *B, int incRowC, int incColC, float* C, float 
 
         : // output
         : // input
-            [A ]"m" (A),      // 0
-            [B] "m" (B),      // 1
-            [C] "m" (C),     // 2
-            [k] "r" (k),       // 3
-            [kr] "r" (kr),      // 4
-            [alpha] "r" (alpha),    // 3
-            [beta]  "r" (beta),      // 4
-            [incRowC]   "r" (incRowC)  // 0
+            [A ]"m" (A),      // x0
+            [B] "m" (B),      // x1
+            [C] "m" (C),     // x2
+            [k] "r" (k),       // x3
+            [kr] "r" (kr),      // x4
+            [alpha] "r" (alpha),    // w3
+            [beta]  "r" (beta),      // w4
+            [incRowC]   "r" (incRowC),  // x0
+            [nextA] "m" (nextA),
+            [nextB] "m" (nextB)
         : // register clobber list
             "memory", "x0", "x1", "x2", "x3",
-            "x4", "v0", "v1", "v2", "v3", "v4",
-            "v10", "v11", "v12", "v13", "w3", "w4"
+            "x4", "x5", "x6", "v0", "v1", "v2", "v3", 
+            "v4", "v10", "v11", "v12", "v13", "w3", "w4"
     );
 }
 
 void macro_kernel(int incRowC, int incColC, float* C, float alpha, float beta){
     int i, j;
+    float *nextA, *nextB;
     int m = MC/MR;
     int n = NC/NR;
     for(i=0; i<m; i++){
+        nextA = &_A[i*KC*MR];
         for(j=0; j<n; j++){
-            micro_kernel(&_A[i*MR*KC], &_B[j*NR*KC], incRowC, incColC, &C[i*MR*incRowC + j*NR*incColC], alpha, beta);
+            nextB = &_B[(j+1)*KC*NR];
+            if (j==n-1){
+                nextB = _B;
+                nextA = &_A[(i+1)*KC*MR];
+                if (i==m-1){
+                    nextA = _A;
+                }
+            }
+            micro_kernel(&_A[i*MR*KC], &_B[j*NR*KC], incRowC, incColC, &C[i*MR*incRowC + j*NR*incColC], alpha, beta, nextA, nextB);
         }
     }
 }
